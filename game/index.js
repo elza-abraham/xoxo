92e060df2af42d6f2ab48b0b7941eff1bc8f673d
elza-abraham <elzaeltho@gmail.com>
2018-03-28 09:05:12 -0700

    MileStone: XOXO on the honor system

--- game/index.js
+++ game/index.js
@@ -1,6 +1,23 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
+export const move = (turn, coords) => ({
+  type: 'MOVE',
+  position: coords,
+  player: turn
+})
+
+export default function reducer(state = {board: Map(), turn: 'X'}, action) {
+  if (action.type === 'MOVE'){
+    let nextTurn = '';
+    if (state.turn === 'X'){
+      nextTurn = 'O'
+    } else {
+      nextTurn = 'X'
+    }
+    return {
+      board: state.board.setIn(action.position, action.player),
+      turn: nextTurn
+    }
+  }
   return state
 }
